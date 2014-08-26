4e160bac7953085ab4d57537674541b5913e7e95
Simon Stenberg <simons@dna.lundberg.gu.se>
2014-08-26 14:15:01 +0200

    Updated filename

--- run_variant_calling.sh
+++ run_variant_calling.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_001_up_062_1_trimmed.fastq 1>01.out 2>01.err
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_002_up_062_2_trimmed_2.fastq 1>02.out 2>02.err
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_003_up_062_3_trimmed_2.fastq 1>03.out 2>03.err
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_004_up_062_4_trimmed.fastq 1>04.out 2>04.err
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_005_up_062_5_trimmed.fastq 1>05.out 2>05.err
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_006_up_062_6_trimmed.fastq 1>06.out 2>06.err
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_007_up_062_7_trimmed_2.fastq 1>07.out 2>07.err
-/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_008_up_062_8_trimmed.fastq 1>08.out 2>08.err
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_009_up_062_9_trimmed.fastq 1>09.out 2>09.err
-#/home/simons/iontorrent_vcalling/freebays_pipeline_3.sh IonXpress_010_up_062_10_trimmed_3.fastq 1>10.out 2>10.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_001_up_062_1_trimmed.fastq 1>01.out 2>01.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_002_up_062_2_trimmed_2.fastq 1>02.out 2>02.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_003_up_062_3_trimmed_2.fastq 1>03.out 2>03.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_004_up_062_4_trimmed.fastq 1>04.out 2>04.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_005_up_062_5_trimmed.fastq 1>05.out 2>05.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_006_up_062_6_trimmed.fastq 1>06.out 2>06.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_007_up_062_7_trimmed_2.fastq 1>07.out 2>07.err
+/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_008_up_062_8_trimmed.fastq 1>08.out 2>08.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_009_up_062_9_trimmed.fastq 1>09.out 2>09.err
+#/home/simons/iontorrent_vcalling/freebays_pipeline.sh IonXpress_010_up_062_10_trimmed_3.fastq 1>10.out 2>10.err
