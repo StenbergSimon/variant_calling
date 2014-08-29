37c265021f348ea10dfd803bae65f332eb58cf2c
Simon Stenberg <simons@dna.lundberg.gu.se>
2014-08-29 13:43:29 +0200

    BUGFIX: Fixed error due to removal of output column in cnv_pipe.pl. Adjusted the column numbers

--- cnv_merger.pl
+++ cnv_merger.pl
@@ -14,6 +14,8 @@ my $increment;
 my $windowsize;
 my @merger;
 
+# Read info about increment, and windowsize
+
 foreach (@table){
 	if ($_ =~ m/(#Increment:)(\d+)/){
 		$increment = $2;
@@ -23,6 +25,7 @@ foreach (@table){
 		}
 	}
 
+# Remove commments for a clean table
 foreach (@table){
         unless ($_ =~ m/^#/){
                 push (@table_clean, $_);
@@ -30,16 +33,22 @@ foreach (@table){
 	}
 my $i = 0;
 
-
+# Foreach line in table clean...
 foreach my $line (@table_clean){
+	# ... Split every line with tab as delimiter
 	my @array = split ("\t", $line);
+	# Push the splitted line (array into an array) -> array of arrays
 	push(@matrix, \@array);
 	}
 my @lib; 
+# When a line in the matrix exists
 while (defined $matrix[$i]){
 	push (@lib, $i);
-	unless(!defined $matrix[($i-1)]){	
-		if (($matrix[$i][0] eq $matrix[($i-1)][0]) && ((($matrix[$i][6])  == ($matrix[($i-1)][6] + $increment))) || (($matrix[$i][6])  == ($matrix[($i-1)][6] + $windowsize))){
+	unless(!defined $matrix[($i-1)]){
+		# Check if chromosome is same (col 0 ) and if previous end coordinate is the same as current end + increment
+		# ...or windowsize	
+		if (($matrix[$i][0] eq $matrix[($i-1)][0]) && ((($matrix[$i][5])  == ($matrix[($i-1)][5] + $increment))) || (($matrix[$i][5])  == ($matrix[($i-1)][5] + $windowsize))){
+			# Store increments
 			my $new = pop(@lib);
 			my $last = pop(@lib);
 			$new = $last . "," . $new;
@@ -52,7 +61,7 @@ while (defined $matrix[$i]){
 $i = 0;
 foreach my $line (@lib){
 	my @log2;
-	my @diff;
+	#my @diff;
 	my @start;
 	my @stop;
 	my @ref_avg;
@@ -66,25 +75,25 @@ foreach my $line (@lib){
 		push (@log2, $matrix[$ref][1]);
 		push (@ref_avg, $matrix[$ref][2]);
 		push (@sam_avg, $matrix[$ref][3]);
-		push (@diff, $matrix[$ref][4]);
-		push (@start, $matrix[$ref][5]);
-		push (@stop, $matrix[$ref][6]);
+	#	push (@diff, $matrix[$ref][4]);
+		push (@start, $matrix[$ref][4]);
+		push (@stop, $matrix[$ref][5]);
 		}
 	foreach (@stop){
 		$_ =~ s/\n//g;
 		}
-		
+	# Sort coordinates	
 	my @start_sort = sort {$a <=> $b} @start;
 	my @stop_sort = sort{$a <=> $b} @stop;
 	
 	my $start = $start_sort[0];
 	my $stop = pop(@stop_sort);
 	my $length = $stop - $start;	
-
-	my $new_entry = $name . "\t" . average(\@log2) . "\t" . average(\@ref_avg) . "\t" .  average(\@sam_avg) . "\t" . average(\@diff) . "\t" . $start . "\t" . $stop . "\t" . $length . "\n";
+	# Prepare new entries with merged 
+	my $new_entry = $name . "\t" . average(\@log2) . "\t" . average(\@ref_avg) . "\t" .  average(\@sam_avg) . "\t" . $start . "\t" . $stop . "\t" . $length . "\n";
 	push (@table_fixed, $new_entry);
 	}
-
+# Printing
 foreach my $line2 (@table){
         if ($line2 =~ m/^#Chrom\/Contig/){
                 $line2 =~ s/\n//g;
@@ -104,7 +113,7 @@ foreach (@table_fixed){
 
 
 
-
+#### Subroutines ####
 
 sub average{
 
