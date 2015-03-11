d861ecb5ea025e44e3c2d3947e52c12a3a9c2daa
StenbergSimon <simon.stenberg@nmbu.no>
2015-03-11 12:05:27 +0100

    Multiple updates

--- freebayes_pipeline.sh
+++ freebayes_pipeline.sh
@@ -76,7 +76,6 @@ rm $filename/bam/${filename}_sorted.bam
 samtools index $filename/bam/${filename}_sorted_RMDUP.bam 
 
 # Add read groups
-# Not really good for anything, GATK just wants it
 
 java -Xmx2g -jar ~/bin/AddOrReplaceReadGroups.jar \
 INPUT=$filename/bam/${filename}_sorted_RMDUP.bam \
@@ -96,37 +95,17 @@ rm $filename/bam/${filename}_sorted_RMDUP.bam
 
 samtools index $filename/bam/${filename}_sorted_RMDUP_rg.bam
 
-# Local realignment 
-
-# Create intervals for local realignment
-
-java -Xmx2g -jar ~/bin/GenomeAnalysisTK.jar \
--T RealignerTargetCreator \
--R $reference \
--I $filename/bam/${filename}_sorted_RMDUP_rg.bam \
--o $filename/${filename}.intervals \
-> $filename/logs/RealignerTargetCreator.log 2>&1
-
-# Run the Local realignment
-
-java -Xmx4g -jar ~/bin/GenomeAnalysisTK.jar \
--T IndelRealigner \
--R $reference \
--I $filename/bam/${filename}_sorted_RMDUP_rg.bam \
--targetIntervals $filename/${filename}.intervals \
--o $filename/bam/${filename}_sorted_RMDUP_rg_realigned.bam \
-> $filename/logs/IndelRealigner.log 2>&1 
-
 # SNP Calling:
 
+samtools calmd -Arb $filename/bam/${filename}_sorted_RMDUP_rg.bam $reference | \
 freebayes \
 --fasta-reference $reference \
 --ploidy 1 \
---no-indels \
--F 0.01 \
+-F 0.1 \
+-no-complex \
 --pooled-continuous \
-$filename/bam/${filename}_sorted_RMDUP_rg_realigned.bam \
-1>$filename/${filename}_snp.vcf 2>$filename/logs/freebays.error 
+--stdin \
+1>$filename/${filename}.vcf 2>$filename/logs/freebays.error 
 
 
 # Annotating SNPs
@@ -140,122 +119,56 @@ yps128_bils \
 -no-intron \
 -no-utr \
 -o gatk \
-$filename/${filename}_varscan_snp_rmf.vcf \
-> $filename/${filename}_varscan_snp_rmf_snpeff.vcf
+$filename/${filename}.vcf \
+> $filename/${filename}_snpeff.vcf
 
-#java -jar ~/bin/GenomeAnalysisTK.jar \
-#-R $reference \
-#-T VariantsToTable \
-#-V $filename/${filename}_varscan_snp_rmf_snpeff.vcf \
-#-F CHROM \
-#-F POS \
-#-F ID \
-#-F REF \
-#-F ALT \
-#-GF DP \
-#-GF SDP \
-#-GF RD \
-#-GF AD \
-#-GF FREQ \
-#-GF GQ \
-#-GF PVAL \
-#-GF RBQ \
-#-GF ABQ \
-#-GF RDF \
-#-GF RDR \
-#-GF ADF \
-#-GF ADR \
-#-o $filename/${filename}_varscan_snp_rmf_snpeff_gatk.table
+#Filter 
 
-#java -Xmx8g -jar ~/bin/SnpSift.jar \
-#extractFields \
-#-V $filename/${filename}_varscan_snp_rmf_snpeff.vcf \
-#CHROM \
-#POS \
-#"EFF[*].GENE" \
-#"EFF[*].EFFECT" \
-#"EFF[*].FUNCLASS" \
-#"EFF[*].AA" \
-#> $filename/${filename}_varscan_snp_rmf_snpeff_sift.table
-#
-#mergeVcfTables.pl $filename/${filename}_varscan_snp_rmf_snpeff_gatk.table $filename/${filename}_varscan_snp_rmf_snpeff_sift.table > $filename/${filename}_varscan_snp_rmf_snpeff_final.table
+java -Xmx8g -jar ~/bin/SnpSift.jar \
+filter \
+'QUAL > 10' \
+$filename/${filename}_snpeff.vcf \
+> $filename/${filename}_filtered_snpeff.vcf
 
+echo $filename/${filename}_filtered_snpeff.vcf >> vcf_list.txt
+echo scp $filename/${filename}_filtered_snpeff.vcf simon@genmac33.gen.gu.se:~/ >> scp_vcf_list.txt
 
-# Filtering indels
+#Convert to table
+java -Xmx8g -jar ~/bin/SnpSift.jar \
+extractFields \
+$filename/${filename}_filtered_snpeff.vcf \
+CHROM \
+POS \
+"EFF[*].GENE" \
+TYPE \
+REF \
+ALT \
+QUAL \
+FILTER \
+DP \
+AF \
+AO \
+RO \
+QA \
+QR \
+DPRA \
+SRP \
+SAP \
+AC \
+AN \
+"EFF[*].EFFECT" \
+"EFF[*].FUNCLASS" \
+"EFF[*].AA" \
+> $filename/${filename}_filtered_variants.table 
 
-#tabix -p vcf $filename/${filename}_indels_rmf.vcf.gz
-#~/bin/bcftools/bcftools filter -o $filename/${filename}_indels_filtered.vcf -i 'FMT/AD/(FMT/RD+FMT/AD)>0.7' $filename/${filename}_indels_rmf.vcf.gz
-#
-#
-#mv snpEff_genes.txt $filename/logs
-#mv snpEff_summary.html $filename/logs
-
-# Annotation of indel .vcf file
-
-#java -Xmx8g -jar ~/bin/snpEff.jar \
-#-v \
-#yps128_bils \
-#-treatAllAsProteinCoding Auto \
-#-no-downstream \
-#-no-intergenic \
-#-no-intron \
-#-no-upstream \
-#-no-utr \
-#$filename/${filename}_indels_filtered_varw.vcf \
-#> $filename/${filename}_indels_filtered_snpEff.vcf
-
-
-# Convert indel vcf to table
-#java -jar ~/bin/GenomeAnalysisTK.jar \
-#-R $reference \
-#-T VariantsToTable \
-#-V $filename/${filename}_indels_filtered_snpEff.vcf \
-#-F CHROM \
-#-F POS \
-#-F ID \
-#-F REF \
-#-F ALT \
-#-GF DP \
-#-GF SDP \
-#-GF RD \
-#-GF AD \
-#-GF GQ \
-#-GF PVAL \
-#-GF RBQ \
-#-GF ABQ \
-#-GF RDF \
-#-GF RDR \
-#-GF ADF \
-#-GF ADR \
-#-GF VARW \
-#-o $filename/${filename}_indels_snpEff_gatk.table
-#
-#java -Xmx8g -jar ~/bin/SnpSift.jar \
-#extractFields \
-#$filename/${filename}_indels_filtered_snpEff.vcf \
-#CHROM \
-#POS \
-#"EFF[*].GENE" \
-#"EFF[*].EFFECT" \
-#"EFF[*].FUNCLASS" \
-#"EFF[*].AA" \
-#> $filename/${filename}_indels_snpEff_sift.table
-#
-#mergeVcfTables.pl $filename/${filename}_indels_snpEff_gatk.table $filename/${filename}_indels_snpEff_sift.table $filename/${filename}_indels_snpEff_gatk.table > $filename/${filename}_indels_filtered_snpEff_final.table
-
-
-mv snpEff_genes.txt snpEff_genes_indel.txt
-mv snpEff_summary.html snpEff_summary_indel.html
-
-mv snpEff_genes_indel.txt $filename/logs
-mv snpEff_summary_indel.html $filename/logs
-
+echo $filename/${filename}_filtered_variants.table >> table_list.txt
+echo scp $filename/${filename}_filtered_variants.table simon@genmac33.gen.gu.se:~/ >> scp_table_list.txt
 
 # Collect GC bias plot
 
 java -Xmx2g -jar ~/bin/CollectGcBiasMetrics.jar \
 REFERENCE_SEQUENCE=$reference \
-INPUT=$filename/bam/${filename}_sorted_RMDUP.bam \
+INPUT= $filename/bam/${filename}_sorted_RMDUP_rg.bam \
 OUTPUT=$filename/${filename}_gcbias.out \
 ASSUME_SORTED=true \
 CHART_OUTPUT=$filename/plots/${filename}_gc_bias_plot.pdf \
@@ -265,7 +178,7 @@ VALIDATION_STRINGENCY=LENIENT \
 # Correct GC bias
 
 automatic_gc_normalization.pl \
-$filename/bam/${filename}_sorted_RMDUP.bam \
+$filename/bam/${filename}_sorted_RMDUP_rg.bam \
 $fragmentlength \
 $regenome2bit \
 1>$filename/logs/${filename}_gc_bias_correction.log \
@@ -275,7 +188,7 @@ $regenome2bit \
 
 java -Xmx2g -jar ~/bin/CollectGcBiasMetrics.jar \
 REFERENCE_SEQUENCE=$reference \
-INPUT=$filename/bam/${filename}_sorted_RMDUP_gc-corrected.bam \
+INPUT=$filename/bam/${filename}_sorted_RMDUP_rg_gc-corrected.bam \
 OUTPUT=$filename/${filename}_gcbias-corrected.out \
 ASSUME_SORTED=true \
 CHART_OUTPUT=$filename/plots/${filename}_gc_bias-corrected_plot.pdf \
@@ -283,38 +196,38 @@ TMP_DIR=$filename/picard_temps \
 VALIDATION_STRINGENCY=LENIENT \
 
 # CNV analysis
-python ~/git/CNV_pipe/cnv_caller.py -f ${filename}/bam/${filename}_sorted_RMDUP_gc-corrected.bam -m 1 -w 100 -o ${filename}/CNV_analysis -l ~/git/CNV_pipe/chr_only_I_XVI.list
+python ~/git/CNV_pipe/cnv_caller.py -f $filename/bam/${filename}_sorted_RMDUP_rg_gc-corrected.bam -m 1 -w 100 -o ${filename}/CNV_analysis -l ~/git/CNV_pipe/chr_only_I_XVI.list
 
-mkdir -p $filename/intermediate_files/
-mv $filename/${filename}.intervals $filename/intermediate_files/
-mv $filename/${filename}_filtered_snp.vcf $filename/intermediate_files/
-mv $filename/${filename}_gcbias-corrected.out $filename/intermediate_files/
-mv $filename/${filename}_gcbias.out $filename/intermediate_files/
-mv $filename/${filename}_indels.vcf.gz $filename/intermediate_files/
-mv $filename/${filename}_indels.vcf.gz.tbi $filename/intermediate_files/
-mv $filename/${filename}_indels_filtered.vcf $filename/intermediate_files/
-mv $filename/${filename}_indels_filtered_snpEff.vcf $filename/intermediate_files/
-mv $filename/${filename}_indels_rmf.vcf.gz $filename/intermediate_files/
-mv $filename/${filename}_indels_rmf.vcf.gz.tbi $filename/intermediate_files/
-mv $filename/${filename}_indels_snpEff_sift.table $filename/intermediate_files/
-#mv $filename/${filename}_snp.vcf.gz $filename/intermediate_files/
-#mv $filename/${filename}_snp.vcf.gz.tbi $filename/intermediate_files/
-#mv $filename/${filename}_snp_rmf.vcf $filename/intermediate_files/
-#mv $filename/${filename}_filtered_snp_snpEFF.vcf $filename/intermediate_files/
-mv $filename/${filename}_indels_snpEff_gatk.table $filename/intermediate_files/
-mv $filename/cnv_analysis_report.tsv $filename/intermediate_files/
-mv $filename/cnv_analysis_report_merged.tsv $filename/intermediate_files/
-mv $filename/cnv_analysis_report_merged_annotated.tsv $filename/intermediate_files/
-mv $filename/GC_bias.out $filename/intermediate_files/
-mv $filename/${filename}_indels_filtered_VARW.vcf.gz $filename/intermediate_files/
-mv $filename/${filename}_indels_filtered_VARW.vcf.gz.tbi $filename/intermediate_files/
-mv $filename/${filename}_indels_filtered_varw.vcf $filename/intermediate_files/
-mv $filename/${filename}__indels_filtered_snpEff.vcf.idx $filename/intermediate_files/
-mv $filename/${filename}_varscan_snp.vcf.gz $filename/intermediate_files/
-mv $filename/${filename}_varscan_snp_rmf.vcf $filename/intermediate_files/
-mv $filename/${filename}_varscan_snp_rmf_snpeff.vcf $filename/intermediate_files/
-mv $filename/${filename}_varscan_snp_rmf_snpeff_gatk.table $filename/intermediate_files/
-mv $filename/${filename}_varscan_snp_rmf_snpeff_sift.table $filename/intermediate_files/
-mv $filename/${filename}_varscan_snp.vcf.gz.tbi $filename/intermediate_files/
-# Done :)
-
+#mkdir -p $filename/intermediate_files/
+#mv $filename/${filename}.intervals $filename/intermediate_files/
+#mv $filename/${filename}_filtered_snp.vcf $filename/intermediate_files/
+#mv $filename/${filename}_gcbias-corrected.out $filename/intermediate_files/
+#mv $filename/${filename}_gcbias.out $filename/intermediate_files/
+#mv $filename/${filename}_indels.vcf.gz $filename/intermediate_files/
+#mv $filename/${filename}_indels.vcf.gz.tbi $filename/intermediate_files/
+#mv $filename/${filename}_indels_filtered.vcf $filename/intermediate_files/
+#mv $filename/${filename}_indels_filtered_snpEff.vcf $filename/intermediate_files/
+#mv $filename/${filename}_indels_rmf.vcf.gz $filename/intermediate_files/
+#mv $filename/${filename}_indels_rmf.vcf.gz.tbi $filename/intermediate_files/
+#mv $filename/${filename}_indels_snpEff_sift.table $filename/intermediate_files/
+##mv $filename/${filename}_snp.vcf.gz $filename/intermediate_files/
+##mv $filename/${filename}_snp.vcf.gz.tbi $filename/intermediate_files/
+##mv $filename/${filename}_snp_rmf.vcf $filename/intermediate_files/
+##mv $filename/${filename}_filtered_snp_snpEFF.vcf $filename/intermediate_files/
+#mv $filename/${filename}_indels_snpEff_gatk.table $filename/intermediate_files/
+#mv $filename/cnv_analysis_report.tsv $filename/intermediate_files/
+#mv $filename/cnv_analysis_report_merged.tsv $filename/intermediate_files/
+#mv $filename/cnv_analysis_report_merged_annotated.tsv $filename/intermediate_files/
+#mv $filename/GC_bias.out $filename/intermediate_files/
+#mv $filename/${filename}_indels_filtered_VARW.vcf.gz $filename/intermediate_files/
+#mv $filename/${filename}_indels_filtered_VARW.vcf.gz.tbi $filename/intermediate_files/
+#mv $filename/${filename}_indels_filtered_varw.vcf $filename/intermediate_files/
+#mv $filename/${filename}__indels_filtered_snpEff.vcf.idx $filename/intermediate_files/
+#mv $filename/${filename}_varscan_snp.vcf.gz $filename/intermediate_files/
+#mv $filename/${filename}_varscan_snp_rmf.vcf $filename/intermediate_files/
+#mv $filename/${filename}_varscan_snp_rmf_snpeff.vcf $filename/intermediate_files/
+#mv $filename/${filename}_varscan_snp_rmf_snpeff_gatk.table $filename/intermediate_files/
+#mv $filename/${filename}_varscan_snp_rmf_snpeff_sift.table $filename/intermediate_files/
+#mv $filename/${filename}_varscan_snp.vcf.gz.tbi $filename/intermediate_files/
+## Done :)
+#
